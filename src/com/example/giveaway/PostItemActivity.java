056ee4694b4346919e0b97624fdb8c51cd5f7e01
jdanielsmx <jdsandoval@gmail.com>
2013-11-28 20:30:33 -0600

    posteditem update
    posteditem update

--- src/com/example/giveaway/PostItemActivity.java
+++ src/com/example/giveaway/PostItemActivity.java
@@ -2,7 +2,10 @@ package com.example.giveaway;
 
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -22,6 +25,12 @@ import android.widget.ImageView;
 
 import com.example.giveaway.MainActivity;
 import com.example.giveaway.MyLocation.LocationResult;
+import com.example.giveaway.posteditemendpoint.Posteditemendpoint;
+import com.example.giveaway.posteditemendpoint.model.PostedItem;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.client.util.DateTime;
+
 
 public class PostItemActivity extends Activity {
 
@@ -41,7 +50,7 @@ public class PostItemActivity extends Activity {
 		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 		TextView textView = new TextView(this);
 		textView.setTextSize(10);
-		textView.setText(message);
+		textView.setText(message);		
 		
 		setContentView(R.layout.activity_post_item);
 		//setContentView(textView);
@@ -129,9 +138,86 @@ public class PostItemActivity extends Activity {
 	    }
 	 }
 	 
-	 public void PostItem(View view){		 
+	 public void PostItem(View view){	
+		 /*
 		Item myItem = new Item(currentLocation.getLatitude(), currentLocation.getLongitude(), 
 				 edtextEmail.getText().toString(), edtextPhone.getText().toString(), bmpCurrentBitmap);
 		
+		PostedItem pItem = new com.example.giveaway.posteditemendpoint.model.PostedItem();
+		pItem.setEmail(myItem.GetEmail());
+		pItem.setImageUrl("");
+		pItem.setLatitude(myItem.GetLatitude());
+		pItem.setLongitude(myItem.GetLongitude());
+		pItem.setPhone(myItem.GetPhone());
+		pItem.setPostingDate(new DateTime(new Date()));
+		
+		Posteditemendpoint.Builder builder = new Posteditemendpoint.Builder(
+				AndroidHttp.newCompatibleTransport(), 
+				new JacksonFactory(), 
+				null);
+		
+		builder = CloudEndpointUtils.updateBuilder(builder);
+		
+		Posteditemendpoint endpoint = builder.build();
+		try {
+			endpoint.insertPostedItem(pItem);
+			textTargetUri.setText("Hola mundo");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			//textTargetUri.setText(e.toString());
+		}*/
+		 
+		 new PostItemTask().execute();
+		 
 	 }
+	 
+	 
+  /**
+   * AsyncTask for calling Mobile Assistant API for posting an item
+   */
+  private class PostItemTask extends AsyncTask<Void, Void, Void> {
+
+    /**
+     * Calls appropriate CloudEndpoint to indicate that user checked into a place.
+     *
+     * @param params the place where the user is checking in.
+     */
+    @Override
+    protected Void doInBackground(Void... params) {
+      
+
+		//Item myItem = new Item(currentLocation.getLatitude(), currentLocation.getLongitude(), 
+			//	 edtextEmail.getText().toString(), edtextPhone.getText().toString(), bmpCurrentBitmap);
+		
+		PostedItem pItem = new com.example.giveaway.posteditemendpoint.model.PostedItem();
+		pItem.setEmail("a@b.com");//myItem.GetEmail());
+		pItem.setImageUrl("myurl");
+		pItem.setLatitude(5d);
+		pItem.setLongitude(6d);
+		pItem.setPhone("1234567890");//myItem.GetPhone());
+		//pItem.setPostingDate(new DateTime(new Date()));
+      
+		Posteditemendpoint.Builder builder = new Posteditemendpoint.Builder(
+				AndroidHttp.newCompatibleTransport(), 
+				new JacksonFactory(), 
+				null);
+		
+		builder = CloudEndpointUtils.updateBuilder(builder);
+		
+		Posteditemendpoint endpoint = builder.build();		
+		
+		try {
+			endpoint.insertPostedItem(pItem).execute();
+			textTargetUri.setText("Hola mundo");
+		} catch (Exception e) {//IOException
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			textTargetUri.setText(e.toString());
+		}
+
+      return null;
+    }
+  }	  
+	  
 }
