096d1e1af554242cb8e4db4313caf540c67e76f5
jdanielsmx <jdsandoval@gmail.com>
2014-01-02 18:14:53 -0600

    some updates
    some updates

--- src/com/example/giveaway/PostItemActivity.java
+++ src/com/example/giveaway/PostItemActivity.java
@@ -1,11 +1,32 @@
 package com.example.giveaway;
 
+
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.util.EntityUtils;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -13,8 +34,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Location;
@@ -41,6 +65,10 @@ public class PostItemActivity extends Activity {
 	EditText edtextEmail;
 	EditText edtextPhone;
 	Button myPostButton;
+	Uri targetUri;
+	String sResultingURL;
+	
+	private String MyAppURL = "http://giveawayprealpha.appspot.com/";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +97,7 @@ public class PostItemActivity extends Activity {
 	{		
 		Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 		startActivityForResult(intent, 0);
-		myLocation.getLocation(this, locationResult);
+		//myLocation.getLocation(this, locationResult);
 	}
 	
 	public LocationResult locationResult = new LocationResult(){
@@ -126,7 +154,7 @@ public class PostItemActivity extends Activity {
 	    super.onActivityResult(requestCode, resultCode, data);
 
 	    if (resultCode == RESULT_OK){
-	     Uri targetUri = data.getData();
+	     targetUri = data.getData();
 	     //textTargetUri.setText(targetUri.toString());
 	     try {
 	      bmpCurrentBitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
@@ -176,7 +204,8 @@ public class PostItemActivity extends Activity {
 			//textTargetUri.setText(e.toString());
 		//}
 		 
-		 new PostItemTask().execute();
+		 //new PostItemTask().execute();
+		 new UploadImageTask().execute(targetUri.toString());
 		 myPostButton = (Button)findViewById(R.id.btnPost);
 		 myPostButton.setEnabled(false);		 
 	 }
@@ -218,15 +247,18 @@ public class PostItemActivity extends Activity {
     protected Long doInBackground(Void... params) {
       
 
-		Item myItem = new Item(currentLocation.getLatitude(), currentLocation.getLongitude(), 
-				 edtextEmail.getText().toString(), edtextPhone.getText().toString(), bmpCurrentBitmap);
+		//Item myItem = new Item(currentLocation.getLatitude(), currentLocation.getLongitude(), 
+			//	 edtextEmail.getText().toString(), edtextPhone.getText().toString(), bmpCurrentBitmap);
+
+		Item myItem = new Item((float)1, (float)1, 
+				 edtextEmail.getText().toString(), edtextPhone.getText().toString(), bmpCurrentBitmap);		
 		
 		PostedItem pItem = new com.example.giveaway.posteditemendpoint.model.PostedItem();
 		GeoPt myPt = new com.example.giveaway.posteditemendpoint.model.GeoPt();
-		myPt.setLatitude((float)currentLocation.getLatitude());
-		myPt.setLongitude((float)currentLocation.getLongitude());
+		myPt.setLatitude((float)1);//currentLocation.getLatitude());
+		myPt.setLongitude((float)1);//currentLocation.getLongitude());
 		pItem.setEmail(myItem.GetEmail());
-		pItem.setImageUrl("myurl");
+		pItem.setImageUrl(sResultingURL);
 		pItem.setLocation(myPt);
 		//pItem.setLatitude(5d);
 		//pItem.setLongitude(6d);
@@ -256,10 +288,152 @@ public class PostItemActivity extends Activity {
     }
     
     protected void onPostExecute(Long result){
+    	if (result == 1){
 		 myPostButton = (Button)findViewById(R.id.btnPost);
-		 myPostButton.setEnabled(true);    	    	
+		 myPostButton.setEnabled(true);		 
+    	}
     }
     
-  }	  
+  }
+  
+  
+  public String getRealPathFromURI(Uri contentUri) {
+	  Cursor cursor = null;
+	  try { 
+	    String[] proj = { MediaStore.Images.Media.DATA };
+	    cursor = getContentResolver().query(contentUri,  proj, null, null, null);
+	    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+	    cursor.moveToFirst();
+	    return cursor.getString(column_index);
+	  } finally {
+	    if (cursor != null) {
+	      cursor.close();
+	    }
+	  }
+	}
+
+  private class UploadImageTask extends AsyncTask<String, Void, Long> {
+
+	    @SuppressWarnings("deprecation")
+		protected Long doInBackground(String... sPicturePath) {
+	    	
+	    	try{
+	    		
+	  	  DefaultHttpClient http_client = new DefaultHttpClient();
+	  	  HttpGet http_get = new HttpGet("http://giveawayprealpha.appspot.com/UploadImageHandler");
+		  HttpResponse response = http_client.execute(http_get);
+		  BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+		  String first_line = reader.readLine();
+		  Log.w("PostPicture", "blob_url: " + first_line);
+		  sResultingURL = first_line;	  	  
+	  	  
+		  HttpPost post = new HttpPost(first_line);
+          //http_client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);          
+		  //MultipartEntityBuilder multipartEntity = MultipartEntityBuilder.create();
+		  //multipartEntity.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+		  //File file = new File(sPicturePath[0]);
+		  //multipartEntity.addPart("file", new FileBody(file));
+		  //post.setEntity(multipartEntity.build());	
+
+		  MultipartEntity entity = new MultipartEntity( HttpMultipartMode.BROWSER_COMPATIBLE );
+		  String mime_type = "image/jpeg";
+		  File file = new File(getRealPathFromURI(targetUri));
+		  
+		  entity.addPart("file", new FileBody( file, mime_type));
+		  //multipartEntity.addPart("file", new FileBody(file));
+		  post.setEntity( entity );
+		  //post.setEntity(multipartEntity.build());			  
+		  
+		  String res = EntityUtils.toString( http_client.execute(post).getEntity(), "UTF-8");
+		  Log.i("PostPicture", res); 
+		  
+		  /*
+	  	  HttpGet http_get = new HttpGet(MyAppURL + "bloburl");
+		  HttpResponse response = http_client.execute(http_get);
+		  BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+		  String first_line = reader.readLine();
+		  Log.w("PostPicture", "blob_url: " + first_line);
+		  sResultingURL = first_line;
+*/
+	    	/*
+		  HttpPost post = new HttpPost(first_line);
+		  http_client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+	*/
+/*
+          HttpPost post = new HttpPost("http://giveawayprealpha.appspot.com/");
+          http_client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+
+
+          String result = EntityUtils.toString( http_client.execute(post).getEntity(), "UTF-8");
+          String actualURL = result.substring(result.indexOf("http://"), result.indexOf("\" method"));
+          Log.w("PostPicture", "url " + actualURL );
+          sResultingURL = actualURL;
+		  
+          
+          post = new HttpPost(actualURL);
+          http_client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);           
+          
+		  MultipartEntityBuilder multipartEntity = MultipartEntityBuilder.create(); 
+		  multipartEntity.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+		  //MultipartEntity entity = new MultipartEntity( HttpMultipartMode.BROWSER_COMPATIBLE );
+
+		 //String mime_type = "image/jpeg";
+		  File file = new File(sPicturePath[0]);
+		  //entity.addPart("file", new FileBody( file, mime_type));
+		  multipartEntity.addPart("file", new FileBody(file));
+		  //post.setEntity( entity );
+		  post.setEntity(multipartEntity.build());	
+
+		  String res = EntityUtils.toString( http_client.execute(post).getEntity(), "UTF-8");
+		  Log.i("PostPicture", res);*/
+		  
+		  return (long)1;
+	    	}catch (Exception e){
+	    		e.printStackTrace();	    		
+	    		return (long)0;
+	    	}
+	    	
+	    }
+
+	    protected void onPostExecute(Long uploadResult) {
+	    	
+	    	if (uploadResult == 1){
+	    		new PostItemTask().execute();
+	    	}
+	        /*myBitmap = returnedBitmap;
+			ImageView i = (ImageView) findViewById(R.id.ivPostedItem);		        
+	        if (myBitmap != null){
+				i.setImageBitmap(myBitmap);					
+	        }
+	        else{
+	        	i.setImageResource(R.drawable.na_1);		        	
+	        }*/
+	    }
+	}  
+  
+  /*
+  private void PostPicture(){
 	  
+	  DefaultHttpClient http_client = new DefaultHttpClient();
+	  HttpGet http_get = new HttpGet(MyAppURL + "bloburl");
+	  HttpResponse response = http_client.execute(http_get);
+	  BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+	  String first_line = reader.readLine();
+	  Log.w("PostPicture", "blob_url: " + first_line);
+
+	  HttpPost post = new HttpPost(first_line);
+	  http_client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+	  MultipartEntity entity = new MultipartEntity( HttpMultipartMode.BROWSER_COMPATIBLE );
+
+	  String filename = null;
+	  String mime_type = "application/zip";
+	  File file = new File( context.getFilesDir(), filename );
+	  entity.addPart( "file", new FileBody( file, mime_type));
+	  post.setEntity( entity );
+
+	  String result = EntityUtils.toString( http_client.execute(post).getEntity(), "UTF-8");
+	  Log.i("PostPicture", result);
+	  	  	  
+  }
+	*/  
 }
